e4e459b792394013faef4ae014ae3923ecb8fd05
KunoVonHagen <--help>
2026-02-11 13:42:49 +0100

    Removed undefined and unused environment variable

--- setup/setup.py
+++ setup/setup.py
@@ -16,8 +16,6 @@ load_dotenv()
 BACKEND_DIR = "/root/ctf-challenger/backend"
 sys.path.append(BACKEND_DIR)
 
-PlATFORM_URL = os.getenv("PlATFORM_URL", None)
-
 PROXMOX_HOST = os.getenv("PROXMOX_HOST", "10.0.0.1")
 PROXMOX_USER = os.getenv("PROXMOX_USER", "root@pam")
 PROXMOX_PASSWORD = os.getenv("PROXMOX_PASSWORD")
